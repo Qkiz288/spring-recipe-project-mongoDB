482e2f68c4e0e41ba6098102b4a57d1bee2479f4
Qkiz288 <qkiz288@gmail.com>
2019-07-29 10:41:02 +0200

    initial commit

---
+++ src/test/java/com/kkukielka/springrecipeproject/SpringRecipeProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kkukielka.springrecipeproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRecipeProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
