dc4706dae3c347bbbbd67b3d14b23f7bd528090a
Qkiz288 <qkiz288@gmail.com>
2019-08-01 15:28:51 +0200

    added querying methods for Category and UnitOfMeasure

--- src/main/java/com/kkukielka/springrecipeproject/controllers/IndexController.java
+++ src/main/java/com/kkukielka/springrecipeproject/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package com.kkukielka.springrecipeproject.controllers;
 
+import com.kkukielka.springrecipeproject.domain.Category;
+import com.kkukielka.springrecipeproject.domain.UnitOfMeasure;
+import com.kkukielka.springrecipeproject.repositories.CategoryRepository;
+import com.kkukielka.springrecipeproject.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
-        System.out.println("......................................... :D");
+
+        Optional<Category> americanCategory = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> teaSpoonUnitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        americanCategory.ifPresent(t -> System.out.println("Category ID: " + t.getId()));
+        teaSpoonUnitOfMeasure.ifPresent(t -> System.out.println("UOM ID: " + t.getId()));
+
         return "index";
     }
 
--- src/main/java/com/kkukielka/springrecipeproject/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/kkukielka/springrecipeproject/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package com.kkukielka.springrecipeproject.repositories;
 import com.kkukielka.springrecipeproject.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
