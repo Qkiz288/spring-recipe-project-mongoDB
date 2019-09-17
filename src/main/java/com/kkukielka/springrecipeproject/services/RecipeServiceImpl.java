9771dcb40f0ecb84344fa9955b0e0f8b6231e0f4
Qkiz288 <qkiz288@gmail.com>
2019-09-17 14:30:15 +0200

    - added RecipeController - Updated RecipeService interface with findById() - implemented findById() method of RecipeService interface - added show.html page for displaying recipe data - added link to recipe in index.html page - added RecipeControllerTest - added findById() unit test in RecipeServiceImplTest - added org.webjars.bootstrap

--- src/main/java/com/kkukielka/springrecipeproject/services/RecipeServiceImpl.java
+++ src/main/java/com/kkukielka/springrecipeproject/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -27,5 +28,16 @@ public class RecipeServiceImpl implements RecipeService {
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
 
         return recipes;
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+
+        if (!recipeOptional.isPresent()) {
+            throw new RuntimeException(String.format("Recipe ID = %s not found", id));
+        }
+
+        return recipeOptional.get();
     }
 }
