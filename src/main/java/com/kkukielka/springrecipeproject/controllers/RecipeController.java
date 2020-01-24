060b9a124e4a952f039ee12b544efc50644af509
Qkiz288 <qkiz288@gmail.com>
2020-01-24 15:43:32 +0100

    [362] Updated notFoundHandler

--- src/main/java/com/kkukielka/springrecipeproject/controllers/RecipeController.java
+++ src/main/java/com/kkukielka/springrecipeproject/controllers/RecipeController.java
@@ -1,13 +1,16 @@
 package com.kkukielka.springrecipeproject.controllers;
 
 import com.kkukielka.springrecipeproject.commands.RecipeCommand;
+import com.kkukielka.springrecipeproject.exceptions.NotFoundException;
 import com.kkukielka.springrecipeproject.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 @Slf4j
 @Controller
@@ -76,21 +79,18 @@ public class RecipeController {
         return "redirect:/";
     }
 
-    // temporarily commented out
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView notFoundHandler(Exception exception) {
-//
-//        log.error("Handling not found error");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("404error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//
-//    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String notFoundHandler(Exception exception, Model model) {
+
+        log.error("Handling not found error");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception", exception);
+
+        return "404error";
+
+    }
 
 }
